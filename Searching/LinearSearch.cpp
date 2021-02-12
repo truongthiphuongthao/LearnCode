5dfbe73728997c3abb61d00faf7b5867855290e6
Thao <thaotruong199826@gmail.com>
2021-02-12 17:12:37 +0700

    linearsearch

---
+++ Searching/LinearSearch.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+int linearSearch(int n, int arr[], int k){
+	for(int i=0; i<n ;i++){
+		if(arr[i] == k){
+			return i;
+		}
+	}
+	return -1;
+}
+int main(){
+	int arr[100];
+	int n;
+	cin >> n;
+	for(int i=0; i<n; i++){
+		cin >> arr[i];
+	}
+	for(int i=0; i<n; i++){
+		cout << arr[i] << " ";
+	}
+	int k;
+	cout << "\n";
+	cin >> k;
+	int result = linearSearch(n, arr, k);
+	if(result != -1){
+		cout << "The element is present at " << (result+1);
+	}
+	else {
+		cout << "The element is not present at " << result;
+	}
+}
